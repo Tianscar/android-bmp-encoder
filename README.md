c471860bb9bed6f3f71593f07394b1df074a5db8
tianscar <tianscar@protonmail.com>
2023-01-10 10:46:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Android BMP Encoder
+The missing encoder to encode android.graphics.Bitmap to Windows Bitmap (*.bmp)
+
+## Add the library to your project (gradle)
+1. Add the Maven Central repository (if not exist) to your build file:
+```groovy
+repositories {
+    mavenCentral()
+}
+```
+
+2. Add the dependency:
+```groovy
+dependencies {
+    implementation 'com.tianscar:android-bmp-encoder:1.0.0'
+}
+```
+
+## Usage
+```java
+BmpEncoder.compress(Bitmap bitmap, Outputstream stream);
+```
+[JavaDocs](/docs/android-bmp-encoder)  
+[A simple example](library/src/androidTest/java/com/tianscar/bmpencoder/EncodeBmpInstrumentedTest.java)
+
+## License
+[MIT](/LICENSE) (c) Tianscar
---
+++ library/src/androidTest/java/com/tianscar/androidbmpencoder/test/EncodeBmpInstrumentedTest.java
@@ -0,0 +1,47 @@
+package com.tianscar.androidbmpencoder.test;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import com.tianscar.android.graphics.BmpEncoder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class EncodeBmpInstrumentedTest {
+
+    @Test
+    public void encodeBmp() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Bitmap bitmap = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(0xFF000000);
+        File outFile = new File(context.getExternalCacheDir(), "test.bmp");
+        try {
+            if (!outFile.exists()) if (!outFile.createNewFile()) return;
+            try (FileOutputStream stream = new FileOutputStream(outFile)) {
+                System.out.println(BmpEncoder.compress(bitmap, stream));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            bitmap.recycle();
+        }
+    }
+
+}
